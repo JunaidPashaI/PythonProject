f21153bb71ca1a0b16f44028eddbb7445047fcb3
Junaid Pasha I <junaidpasha96@gmail.com>
2026-02-20 22:14:44 +0530

    Initial commit - added python learning files

---
+++ Pyhton_learning/print_prog.py
@@ -0,0 +1,25 @@
+# print("Hello, World! Welcome to Python.")
+
+#program2
+# print("Twinkle, twinkle, little star,\n How I wonder what you are!")
+
+#program3
+# age= 32
+# hight = 5.8
+# student = True
+
+# print(f"Hi Im junaid , and my age is {age}, and my hight is {hight} and iam student {student}")
+
+# Program4 and program5 together
+# a = int(input("enter the number to perfor addition"))
+# b=20
+# print("additon of a and b =", a+b)
+
+#program5
+a= int(input("Enter the First number : "))
+b= int(input("Enter the Second number : "))
+
+print(" a+b is ", a+b)
+print(" a-b is ", a-b)
+print(" a*b is ", a*b)
+print(" a/b is ", a/b)
